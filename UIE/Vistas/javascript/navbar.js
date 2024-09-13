1a1fc79e44ded5930a9050a519f414f93dae0e29
Elias Contrera <wooziecentury@gmail.com>
2024-09-13 17:13:41 -0300

    V1.0 - Software Turismo
    - Primera versión de Interfaz de usuario
    - Pagina de Inicio implementada
    - Separación por Modelado en Capas y Patrón de diseño MVC.
    - Implementación de estilos en la página.

---
+++ UIE/Vistas/javascript/navbar.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
